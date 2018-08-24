aec6676be86c069b12fd36b055b7b7516118c360
Omer Katz <omerk2511@gmail.com>
2018-08-24 15:30:38 +0300

    Updated the Image field of Book to ImageURL and added a validator.

--- models/book.model.js
+++ models/book.model.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 const bookSchema = mongoose.Schema({
     Title: String,
     SubTitle: String,
-    Image: String,
+    ImageURL: {
+        type: String,
+        validate: {
+            validator: v => {
+                return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(v);
+            },
+            message: v => {
+                return v + ' is not a valid URL!';
+            }
+        }   
+    },
     Categories: [String],
     Authors: [String],
     ISBN: String
