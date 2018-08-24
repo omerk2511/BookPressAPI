3efa6b1a93b62eb7a0d06c1e016ffdb6bcd93beb
Omer Katz <omerk2511@gmail.com>
2018-08-24 12:06:33 +0300

    Created a connection to the database.

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const db = require('./config/db.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db.url,  {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database.");
+}).catch(err => {
+    console.log('Could not connect to the database.');
+    process.exit();
+});
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Some initial stuff' });
 });
