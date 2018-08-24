a55c5df86b55ad55707a4b234b6ae8a7453ab580
Omer Katz <omerk2511@gmail.com>
2018-08-24 19:29:49 +0300

    Created a basic books controller.

---
+++ controllers/books.controller.js
@@ -0,0 +1,9 @@
+const Book = require('../models/book.model');
+const User = require('../models/user.model');
+
+const verifyToken = require('../common/verify-token.function');
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
