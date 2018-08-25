16766aeb2db947b450a6071f59e4aef5b1f5857d
Omer Katz <omerk2511@gmail.com>
2018-08-25 10:17:31 +0300

    Added creating book with POSTing /api/books.

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -4,6 +4,29 @@ const User = require('../models/user.model');
 const verifyToken = require('../common/verify-token.function');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const router = express.Router();
+
+router.post('/', verifyToken, (req, res, next) => {
+    Book.create({
+        Title: req.body.Title,
+        SubTitle: req.body.SubTitle,
+        ImageURL: req.body.ImageURL,
+        Categories: req.body.Categories,
+        Authors: req.body.Authors,
+        ISBN: req.body.ISBN
+    }, (err, book) => {
+        if(err)
+            return res.status(500).json({ Message: 'There was a problem creating a book.' });
+
+        User.findOneAndUpdate({ _id: req.userId }, { $push: { Books: mongoose.Types.ObjectId(book._id) } }, (err2, doc, user) => {
+            if(err2)
+                return res.status(500).json({ Message: 'There was a problem assigning the book to the user.' });
+
+            return res.status(201).json(book);
+        });
+    });
+});
 
 module.exports = router;
